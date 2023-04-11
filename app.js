b22455b95f7bf1a3184ac18a318f8bff83a3a057
Myra Smith <myra.fowlkes.smith@gmail.com>
2023-04-11 13:15:09 -0400

    adds Create Bookmark route and bookmark validator middleware

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const bookmarksController = require("./controllers/bookmarksController.js");
 
 // CONFIGURATION
 const app = express();
+app.use(express.json()); // json middleware parses incoming requests with JSON payloads
 
 // ROUTES
 app.get("/", (req, res) => {
   res.send("Welcome to Bookmarks App");
 });
 
-app.use("/bookmarks", bookmarksController); //app use mounts a specified middleware function/s at a specified path
-// app.use("/movies", moviesController)
+app.use("/bookmarks", bookmarksController);
 
 app.get("*", (req, res) => {
-    res.status(404).json({error : "Page Not Found"})
-})
+  console.log("404!");
+  res.status(404).json({ error: "Page Not Found" });
+});
 
 // EXPORT
 module.exports = app;
--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const bookmarks = express.Router();
 const bookmarksArray = require("../models/bookmarks.js");
+const { bookmarkValidator } = require("../models/validators.js");
 
 // index - all bookmarks
-bookmarks.get("/", (req, res) => { // /bookmarks
-  // /bookmarks/
+bookmarks.get("/", (req, res) => {
+  console.log("GET /bookmarks");
+
   res.json(bookmarksArray);
 });
 
-// show - get one
-bookmarks.get("/:index", (req, res) => { // /bookmarks/:index
-  const { index } = req.params;
-  res.json(bookmarksArray[index]);
-}); // /bookmarks/:index
+// create - add a new bookmark
+bookmarks.post("/", bookmarkValidator, (req, res) => {
+  console.log("POST /bookmarks", req.body);
 
+  // In the future, a SQL Database will replace this array
+  bookmarksArray.push(req.body);
+  res.status(201).json(bookmarksArray[bookmarksArray.length - 1]);
+});
+
+// show - get one
+bookmarks.get("/:index", (req, res) => {
+  const { index } = req.params;
+  console.log("GET /bookmarks/:index", index);
+
+  if (bookmarksArray[index]) {
+    res.json(bookmarksArray[index]);
+  } else {
+    res.status(404).json({ error: "Bookmark Not Found" });
+  }
+});
 
 module.exports = bookmarks;
